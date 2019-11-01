1209d7efed74a2bd895a4527fe231114d92e7a97
Sebastien Tricaud <sebastien.tricaud@devo.com>
2019-11-01 12:04:40 -0700

    Removing useless file, now replaced with MakeVariables plugins

--- PCraft/plugins/Cheat.py
+++
@@ -1,9 +0,0 @@
-class PCraftPlugin(object):
-    name = "GenerateNewDomain"
-    
-    def __init__(self, plugins_data):
-        self.plugins_data = plugins_data
-
-    def run(self, script=None):
-        self.plugins_data._set("domain", "fred-replaceme.com")
-        return script["_next"], self.plugins_data
