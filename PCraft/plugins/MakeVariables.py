96851e9c6ad71bfa660ae7430d2e933169b358ea
Sebastien Tricaud <sebastien.tricaud@devo.com>
2019-11-01 11:40:36 -0700

    Adding a new Plugin to build Variables from Scratch, so one can skip the previous plugin

---
+++ PCraft/plugins/MakeVariables.py
@@ -0,0 +1,38 @@
+class PCraftPlugin(object):
+    name = "MakeVariables"
+    
+    def __init__(self, plugins_data):
+        self.plugins_data = plugins_data
+
+    def help(self):
+        helpstr="""
+MakeVariables
+=============
+
+This Plugins creates variables from scractch based on the [pcraft Taxonomy][taxonomy].
+
+Example
+-------
+
+To replace the two variables "ip-dst" and "domain" that any previous plugins may have
+built, the following can be written:
+
+```
+buildvars:
+  _plugin: MakeVariables
+
+  ip-dst: "1.1.1.1"
+  domain: "www.example.com"
+
+  _next: dnsconnect
+```
+
+[taxonomy]:taxonomy.md
+"""
+        
+    def run(self, script=None):
+        for k, v in script.items():
+            if not k.startswith("_"):
+                self.plugins_data._set(k, v)
+
+        return script["_next"], self.plugins_data
