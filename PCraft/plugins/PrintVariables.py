377f735f6a99da7849e6972ea7a7937f77645215
Sebastien Tricaud <sebastien.tricaud@devo.com>
2019-11-19 17:57:30 -0800

    Adding the plugin that prints the variables

---
+++ PCraft/plugins/PrintVariables.py
@@ -0,0 +1,25 @@
+class PCraftPlugin(object):
+    name = "PrintVariables"
+    
+    def __init__(self, plugins_data):
+        self.plugins_data = plugins_data
+
+    def help(self):
+        helpstr="""
+This Plugins prints all the variables
+
+### Example
+
+The usage is trivial, just call it in your flow.
+
+```
+printvars:
+  _plugin: PrintVariables
+
+  _next: dnsconnect
+```
+"""
+        return helpstr
+        
+    def run(self, script=None):
+        print(str(self.plugins_data))
