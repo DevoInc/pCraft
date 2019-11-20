99dc148f2d78058deafd1cb3af16d6837428556c
Sebastien Tricaud <sebastien.tricaud@devo.com>
2019-11-20 10:45:11 -0800

    Adding hostname generation from a function

---
+++ PCraft/functions/hostname.py
@@ -0,0 +1,40 @@
+import struct
+import socket
+import sys
+
+vowels = ['a','e','i','o','u','y','a','e','i','o']
+consonants = ['p','b','c','z','m','f','d','s','t','r']
+
+class PCraftFunction(object):
+    name = "hostname"
+    
+    def help(self):
+        helpstr="""
+Generates a Hostname from an IP address. The name will be consistent.
+"""
+        return helpstr
+
+    def __init__(self):
+        pass
+
+    def run(self, scenariofile, arguments):
+        ipaddr = str(arguments)
+
+        try:
+            iptoint=struct.unpack("!I", socket.inet_pton(socket.AF_INET, ipaddr))[0]
+        except OSError: # illegal IP address string passed to inet_pton
+            iptoint=struct.unpack("!IIII", socket.inet_pton(socket.AF_INET6, ipaddr))[0]
+    
+        ipstr = str(iptoint)
+        outstr = ""
+        pos = 0
+        for i in ipstr:
+            n = int(i)
+            if pos % 2:
+                outstr += vowels[n]
+            else:
+                outstr += consonants[n]
+
+            pos += 1
+        
+        return outstr
