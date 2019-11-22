60fbf0370918177618fd40f062623495128cf0c9
Sebastien Tricaud <sebastien.tricaud@devo.com>
2019-11-21 21:49:47 -0800

    Using the http request from scapy instead of making it from a string.
    Calculating the tcp sequence number properly to keep http traffic out.

--- PCraft/plugins/Controller.py
+++ PCraft/plugins/Controller.py
@@ -1,4 +1,5 @@
 from scapy.all import Ether, IP, TCP
+from scapy.layers import http
 from . import _utils as utils
 import random
 import os
@@ -19,14 +20,25 @@ If you do not know what this it, do not use it.
         return helpstr
     
     def __init__(self, plugins_data):
-        self.plugins_data = plugins_data
-
-    def run(self, script=None):
-
-        httpget_string = "GET {uri} HTTP/1.1\r\nAccept: */*\r\nUser-Agent: {useragent}\r\n\r\n".format(uri="/" + script["log_plugin"], useragent=script["kvdata"])
+        self.plugins_data = plugins_data        
         
-        httpreq = Ether() / IP(src="10.10.10.10",dst="10.10.10.10") / TCP(sport=666,dport=666) / httpget_string
+    def run(self, script=None):
+        try:
+            seq = self.plugins_data._get("seq")
+        except KeyError:
+            seq = 0
+            
+        req = http.HTTPRequest(
+            Path=b'/' + bytes(script["log_plugin"].encode("utf-8")),
+            User_Agent=b'' + bytes(script["kvdata"].encode("utf-8"))
+        )
+        httpreq = Ether() / IP(src="10.10.10.10",dst="10.10.10.10") / TCP(sport=666,dport=666, flags="P""A", seq=seq) / req
         self.plugins_data.pcap.append(httpreq)
+
+        seq += len(httpreq['TCP'].payload)
+        if seq > 2147483647: # 2^32 - 1
+            seq = 0
+        self.plugins_data._set("seq", seq)
         
         return script["_next"], self.plugins_data
 
