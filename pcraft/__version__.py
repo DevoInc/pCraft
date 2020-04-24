b15b643224ec573e918d60700f6629833a31c4a2
Sebastien Tricaud <sebastien.tricaud@devo.com>
2020-04-23 17:26:46 -0700

    Adding a setup.py and version management file

---
+++ pcraft/__version__.py
@@ -0,0 +1,10 @@
+"""Version and details for pcraft"""
+
+__description__ = "Pcraft"
+__url__ = "https://www.github.com/devoinc/pcraft"
+__version__ = "0.0.7"
+__author__ = "Sebastien Tricaud"
+__author_email__ = "sebastien.tricaud@devo.com"
+__license__ = "MIT"
+__maintainer__ = __author__
+__maintainer_email__ = __author_email__
---
+++ setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+import setuptools
+from distutils.core import setup
+import os
+import textwrap
+
+current_dir = os.path.abspath(os.path.dirname(__file__))
+
+about = {}
+with open(os.path.join(current_dir, "pcraft", "__version__.py"), "r") as fp:
+          exec(fp.read(), about)
+
+setup(name="pcraft",
+      version=about["__version__"],
+      author=about["__author__"],
+      author_email=about["__author_email__"],
+      description=about["__description__"],
+      long_description=textwrap.dedent(open("README.md", "r").read()),
+      long_description_content_type="text/markdown",
+      maintainer=about["__maintainer__"],
+      maintainer_email=about["__maintainer_email__"],
+      url=about["__url__"],
+      license=about["__license__"],
+      classifiers=[
+          "Programming Language :: Python :: 3",
+          "License :: OSI Approved :: MIT License",
+          "Operating System :: OS Independent",
+      ],
+      install_requires=[
+          "scapy",
+          "IPy",
+          "PyYAML",
+          "dnspython",
+      ],
+      scripts=["pcrafter"],
+      packages=["pcraft"])
