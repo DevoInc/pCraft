ae2b0f9040fa25e0b55a01d78642b8207129e674
Sebastien Tricaud <sebastien.tricaud@devo.com>
2020-06-03 13:26:04 -0700

    Improve Faker with more names, and avoid name duplicates

--- pcraft/plugins/FakeNames.py
+++ pcraft/plugins/FakeNames.py
@@ -21,13 +21,26 @@ newfake:
 
     def __init__(self, session, plugins_data):
         super().__init__(session, plugins_data)
-        self.faker = Faker()
+        self.faker = Faker(['it_IT', 'en_US', 'es_ES', 'fr_FR', 'de_DE', 'en_GB'])
+        self.fakenames = {}
         
     def run(self, script=None):
-        name = self.faker.name()
-        nametable = name.split(" ") # Sometimes there are spaces in last names, we cut short and only grab the first one
+        no_infinite_loop = 0
+        # We have see some duplicate names. We try 10 times to avoid spending too much time
+        while no_infinite_loop < 10:
+            name = self.faker.name()
+            nametable = name.split(" ") # Sometimes there are spaces in last names, we cut short and only grab the first one
+            first_last = "%s %s" % (nametable[0], nametable[1])        
+            try:
+                a = self.fakenames[first_last]
+                break
+            except KeyError: # Same player plays again!
+              pass
+            
+            no_infinite_loop += 1
+            
         self.setvar("firstname", nametable[0])
-        self.setvar("lastname", nametable[1])
+        self.setvar("lastname", nametable[1])        
         self.setvar("name", name)
 
         self.set_value_or_default(script, "orgdomain", "yoda.com")
