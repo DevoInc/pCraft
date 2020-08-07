e3b8789eb1f669a8d6e64e6c7492eea245445b43
Sebastien Tricaud <sebastien.tricaud@devo.com>
2020-08-06 20:45:53 -0700

    Push new version

--- pcraft/__version__.py
+++ pcraft/__version__.py
@@ -2,7 +2,7 @@
 
 __description__ = "Pcraft"
 __url__ = "https://www.github.com/devoinc/pcraft"
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 __author__ = "Sebastien Tricaud"
 __author_email__ = "sebastien.tricaud@devo.com"
 __license__ = "MIT"
