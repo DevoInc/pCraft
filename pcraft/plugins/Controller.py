a1c6fcff8900a285f08bd86a4674663d5bb42425
Sebastien Tricaud <sebastien.tricaud@devo.com>
2020-11-04 17:41:22 -0800

    Using controller with AMI

--- pcraft/plugins/Controller.py
+++ pcraft/plugins/Controller.py
@@ -23,25 +23,28 @@ If you do not know what this it, do not use it.
     
     def __init__(self, app, session, plugins_data):
         super().__init__(app, session, plugins_data)
+        self.seq = 0
         
-    def run(self, script=None):
-        self.update_vars_from_script(script)
-        try:
-            seq = self.plugins_data._get("seq")
-        except KeyError:
-            seq = 0
-            
+    def run(self, ami, action):
+        kvstring=""
+        afa = action.FieldActions()
+        for field, action in afa.items():
+            for actiontype, actionval in action.items():
+                if actiontype == "set":
+                    for k, v in actionval.items():
+                        kvstring += "%s='''%s''' " % (field,k)
+                        
+        
         req = http.HTTPRequest(
-            Path=b'/' + bytes(script["log_plugin"].encode("utf-8")),
-            User_Agent=b'' + bytes(script["kvdata"].encode("utf-8"))
+            Path=b'/' + bytes(self.getvar("log_plugin").encode("utf-8")),
+            User_Agent=b'' + bytes(kvstring.encode("utf-8"))
         )
-        httpreq = Ether() / IP(src="10.10.10.10",dst="10.10.10.10") / TCP(sport=666,dport=666, flags="P""A", seq=seq) / req
+        httpreq = Ether() / IP(src="10.10.10.10",dst="10.10.10.10") / TCP(sport=666,dport=666, flags="P""A", seq=self.seq) / req
         self.plugins_data.pcap.append(httpreq)
 
-        seq += len(httpreq['TCP'].payload)
-        if seq > 2147483647: # 2^32 - 1
-            seq = 0
-        self.plugins_data._set("seq", seq)
+        self.seq += len(httpreq['TCP'].payload)
+        if self.seq > 2147483647: # 2^32 - 1
+            self.seq = 0
         
-        return script["_next"], self.plugins_data
+        return self.plugins_data
 
