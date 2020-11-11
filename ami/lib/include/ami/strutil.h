2034f69652f0c8651b6c3c527968b4d1221b8e66
Sebastien Tricaud <sebastien.tricaud@devo.com>
2020-11-10 21:22:39 -0800

    Adding an strutil function to replace substrings from a string

---
+++ ami/lib/include/ami/strutil.h
@@ -0,0 +1,14 @@
+#ifndef _AMI_STRUTIL_H_
+#define _AMI_STRUTIL_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+char *ami_strutil_replace_all_substrings(const char *haystack, const char *needle, const char *replacement);
+  
+#ifdef __cplusplus
+}
+#endif
+
+#endif // _AMI_STRUTIL_H_
