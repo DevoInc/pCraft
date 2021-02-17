0940cf46ecb3e0c1ae53c0c2f968a2071031e461
Sebastien Tricaud <sebastien.tricaud@devo.com>
2021-02-17 10:53:18 -0800

    Adding hbbs plugins

---
+++ logwriter/plugins/hbss-agent.py
@@ -0,0 +1,27 @@
+import random
+from datetime import datetime
+import uuid
+
+from logwriter.LogContext import LogContext
+
+class LogPlugin(LogContext):
+    name = "mcafee_agent"
+    active_layer = "no-active-layer"
+
+    def __init__(self, outpath):
+        super().__init__(outpath)
+        self.log_fp = self.openlog("hbss_agent.log")
+
+    def __del__(self):
+        self.closelog()
+
+    def template_to_log(self, packet, kvdict):
+        frame_time = datetime.fromtimestamp(int(float(packet.sniff_timestamp)))
+
+        event = self.retrieve_template("mcafee.hbss", "agent", kvdict)
+        event = frame_time.strftime(event)
+
+        return event
+
+    def run(self, cap, packet, pktid, kvdict):
+        self.log_fp.write(self.template_to_log(packet, kvdict))
---
+++ logwriter/plugins/hbss-intrusion.py
@@ -0,0 +1,27 @@
+import random
+from datetime import datetime
+import uuid
+
+from logwriter.LogContext import LogContext
+
+class LogPlugin(LogContext):
+    name = "mcafee_intrusion"
+    active_layer = "no-active-layer"
+
+    def __init__(self, outpath):
+        super().__init__(outpath)
+        self.log_fp = self.openlog("hbss_intrusion.log")
+
+    def __del__(self):
+        self.closelog()
+
+    def template_to_log(self, packet, kvdict):
+        frame_time = datetime.fromtimestamp(int(float(packet.sniff_timestamp)))
+
+        event = self.retrieve_template("mcafee.hbss", "intrusion", kvdict)
+        event = frame_time.strftime(event)
+
+        return event
+
+    def run(self, cap, packet, pktid, kvdict):
+        self.log_fp.write(self.template_to_log(packet, kvdict))
