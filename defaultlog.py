a30d69f9e18b395e55b65089a40d743b46733073
Sebastien Tricaud <sebastien.tricaud@devo.com>
2021-03-06 18:51:29 -0800

    Adding epo template for hbss

--- defaultlog.py
+++ defaultlog.py
@@ -6,13 +6,18 @@ from logwriter.TemplateBuilder import TemplateBuilder
 
 
 if len(sys.argv) < 3:
-    print("%s template event" % sys.argv[0])
+    print("%s template event_type" % sys.argv[0])
     sys.exit(1)
 
+template = sys.argv[1]
+event_type = sys.argv[2]
+    
 tb = TemplateBuilder()
 
-if tb.check_coverage(sys.argv[1], sys.argv[2]):
-    event = tb.get_event(sys.argv[1], sys.argv[2], None)
+print("Template keys are: %s" % str(tb.get_template_keys(template, event_type)))
+
+if tb.check_coverage(template, event_type):
+    event = tb.get_event(template, event_type, None)
     et = datetime.now()
     event = et.strftime(event)
     print(event)
