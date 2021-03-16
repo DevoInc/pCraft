2b658b8947f012e49028ee989feffc83af559c7e
Sebastien Tricaud <sebastien.tricaud@devo.com>
2021-03-16 13:44:49 -0700

    s/db/template

--- logwriter/plugins/mswin-security.py
+++ logwriter/plugins/mswin-security.py
@@ -40,4 +40,4 @@ class LogPlugin(LogContext):
         
     def run_buffer(self, action, event_time, kvdict):        
         frame_time = datetime.fromtimestamp(event_time)
-        return self.db_to_log(frame_time, kvdict)
+        return self.template_to_log(frame_time, kvdict)
