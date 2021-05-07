d8ec0ccd65848b77a6a9025b3597a91c288f9d4f
Sebastien Tricaud <sebastien.tricaud@devo.com>
2021-05-07 06:04:23 +0000

    Add the $onlyreplace variable which can make the PcapImport import only the IPs we are replacing.

--- pcraft/plugins/PcapImport.py
+++ pcraft/plugins/PcapImport.py
@@ -53,6 +53,12 @@ action importphishing {
         else:
             append_sleep = action.GetSleepCursor() - first_time
 
+        only_replace = None
+        try:
+            only_replace = action.Variables()["$onlyreplace"]
+        except:
+            pass
+            
         amifile = ami.GetFilePath()
         amipath = os.path.dirname(os.path.realpath(amifile))
         pcap_in = os.path.join(amipath, action.Variables()["$filename"])
@@ -99,6 +105,7 @@ action importphishing {
             if ip_list:
                 ip_to_replace = None
                 for k, v in ip_list.items():
+                    we_replaced = False
                     ip_to_replace = k
                     ip_replacement = IP_y(v)
 
@@ -106,12 +113,19 @@ action importphishing {
                         if packet[IP].src == ip_to_replace:
                             packet[IP].src = str(ip_replacement)
                             n_items_replaced += 1
+                            we_replaced = True
                         if packet[IP].dst == ip_to_replace:
                             packet[IP].dst = str(ip_replacement)
                             n_items_replaced += 1
-                            
-                self.plugins_data.AddPacket(action, packet)
-                packets_injected += 1
+                            we_replaced = True
+
+                if we_replaced and only_replace:
+                    self.plugins_data.AddPacket(action, packet)
+                    packets_injected += 1
+                else:
+                    if not only_replace:
+                        self.plugins_data.AddPacket(action, packet)
+                        packets_injected += 1
 
                     # seq += 1
                 last_packet = packet
