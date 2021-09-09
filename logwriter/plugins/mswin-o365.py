040cc4f75a2e49e29b129deb8c4155f5ca62f40c
Sebastien Tricaud <sebastien.tricaud@devo.com>
2021-09-09 11:04:14 -0700

    Adding the mswin o365 login from azure.

--- logwriter/plugins/mswin-o365.py
+++ logwriter/plugins/mswin-o365.py
@@ -7,6 +7,7 @@ from logwriter.LogContext import LogContext
 class LogPlugin(LogContext):
     name = "mswin_o365"
     active_layer = "no-active-layer"
+    logcalls = ["Auth.Login"]
 
     def __init__(self, outpath):
         super().__init__(outpath)
@@ -28,9 +29,38 @@ class LogPlugin(LogContext):
         frame_time = datetime.fromtimestamp(int(float(packet.sniff_timestamp)))
         self.log_fp.write(self.template_to_log(frame_time, kvdict))
 
-    def run_ccraft(self, event, kvdict):
-        frame_time = datetime.fromtimestamp(int(event["time"]))
-        self.log_fp.write(self.template_to_log(frame_time, kvdict))
+    def run_ccraft(self, event, kvdict, logcall=None):
+
+        if logcall:
+            if logcall == "Auth.Login":
+                frame_time = datetime.fromtimestamp(int(event["time"]))
+                try:                    
+                    kvdict["UserId"] = kvdict["$username"]
+                except:
+                    pass
+                try:                  
+                    kvdict["Actor_1_ID"] = kvdict["$username"]
+                except:
+                    pass
+                try:                    
+                    kvdict["Actor_0_ID"] = str(uuid.uuid5(uuid.NAMESPACE_DNS, kvdict["$username"]))
+                except:
+                    pass
+                try:                    
+                    kvdict["ActorIpAddress"] = kvdict["$ip-src"]
+                except:
+                    pass
+                try:                    
+                    kvdict["ClientIP"] = kvdict["$ip-src"]
+                except:
+                    pass
+                
+                event = self.retrieve_template("microsoft.azure.activedirectory", "UserLoggedIn", kvdict)
+                event = frame_time.strftime(event)
+                self.log_fp.write(event)
+            else:
+                frame_time = datetime.fromtimestamp(int(event["time"]))
+                self.log_fp.write(self.template_to_log(frame_time, kvdict))
                 
     def run_buffer(self, action, event_time, kvdict):        
         frame_time = datetime.fromtimestamp(event_time)
