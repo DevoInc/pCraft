a7a8ed5d90948825601e6ec9d7cf079738d30090
Sebastien Tricaud <sebastien.tricaud@devo.com>
2021-10-25 20:33:38 -0700

    Moving bin/ into lib/

---
+++ pkg/available/TcpFlags/lib/tcprst.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+from scapy.all import Ether, IP, TCP
+
+from pcraft import io as PcraftIO
+from pcraft.LibraryContext import *
+
+class PcraftPcapWriter(LibraryContext):
+    def __init__(self):
+        super().__init__()
+
+    def run(self, event):        
+        self.set_vardict(event["variables"])
+        
+        syn = Ether() / IP(src=self.get_variable("$ip-src"), dst=self.get_variable("$ip-dst")) / TCP(dport=int(self.get_variable("$port-dst")), sport=int(self.get_variable("$port-src")), flags="S")
+        pkt = PcraftIO.raw_packet_from_scapy(syn)
+        yield "network", pkt
+
+        
+        rst_ack = Ether() / IP(src=self.get_variable("$ip-dst"), dst=self.get_variable("$ip-src")) / TCP(sport=syn[TCP].dport, dport=syn[TCP].sport, flags="R""A")
+        pkt = PcraftIO.raw_packet_from_scapy(rst_ack)
+        yield "network", pkt
---
+++ pkg/available/TcpFlags/lib/tcpsynack.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+from scapy.all import Ether, IP, TCP
+
+from pcraft import io as PcraftIO
+from pcraft.LibraryContext import *
+
+class PcraftPcapWriter(LibraryContext):
+    def __init__(self):
+        super().__init__()
+
+    def run(self, event):
+        self.set_vardict(event["variables"])
+        
+        syn, syn_ack, ack = utils.get_tcp_three_way_handshake(self.get_variable("$ip-src"),
+                                                              self.get_variable("$ip-dst"),
+                                                              int(self.get_variable("$port-src")),
+                                                              int(self.get_variable("$port-dst")))
+
+        pkt = PcraftIO.raw_packet_from_scapy(syn)
+        yield "network", pkt
+        pkt = PcraftIO.raw_packet_from_scapy(syn_ack)
+        yield "network", pkt
+        pkt = PcraftIO.raw_packet_from_scapy(ack)
+        yield "network", pkt
+
+
