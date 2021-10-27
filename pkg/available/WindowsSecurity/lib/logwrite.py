43c29f25cb62c7acdbbd3c257ec35c04075cb5ee
Sebastien Tricaud <sebastien.tricaud@devo.com>
2021-10-27 10:00:50 -0700

    Set variables for user is they aren't set

--- pkg/available/WindowsSecurity/lib/logwrite.py
+++ pkg/available/WindowsSecurity/lib/logwrite.py
@@ -8,9 +8,21 @@ from pcraft.TemplateBuilder import template_get_event
 class PcraftLogWriter(LibraryContext):
     def __init__(self):
         super().__init__()
-
+        
     def run(self, event, config, templates):
         frame_time = datetime.fromtimestamp(event["time"])
+
+        username = None
+        try:
+            username = event["variables"]["$username"]
+        except:
+            pass
+
+        if username:
+            if not "$winlog_event_data_SubjectUserName" in event["variables"]:
+                event["variables"]["$winlog_event_data_SubjectUserName"] = username
+            if not "$winlog_event_data_TargetUserName" in event["variables"]:
+                event["variables"]["$winlog_event_data_TargetUserName"] = username
         
         event = template_get_event(templates, event["variables"]["$event_id"], event["variables"])
         event = frame_time.strftime(event)
