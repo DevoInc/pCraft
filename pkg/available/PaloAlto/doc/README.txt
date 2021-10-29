3b9804465f46a87b0ac645bdad2327bde35dd991
Sebastien Tricaud <sebastien.tricaud@devo.com>
2021-10-29 11:27:20 -0700

    Adding Palo Alto package

---
+++ pkg/available/PaloAlto/doc/README.txt
@@ -0,0 +1,5 @@
+paloalto.firewall.v9:
+https://docs.paloaltonetworks.com/pan-os/9-0/pan-os-admin/monitoring/use-syslog-for-monitoring/syslog-field-descriptions/traffic-log-fields.html
+
+paloalto.firewall.v9_1:
+https://docs.paloaltonetworks.com/pan-os/9-1/pan-os-admin/monitoring/use-syslog-for-monitoring/syslog-field-descriptions/traffic-log-fields.html
---
+++ pkg/available/PaloAlto/lib/logwrite.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+import json
+from datetime import datetime
+
+from pcraft.LibraryContext import *
+from pcraft.TemplateBuilder import template_get_event, template_get_header
+
+class PcraftLogWriter(LibraryContext):
+    def __init__(self):
+        super().__init__()
+        self.is_first = True
+
+    def run(self, event, config, templates):
+        event_name = "traffic"
+        if self.is_first:
+            self.is_first = False
+            header = template_get_header(templates, event_name)
+            yield bytes(header, "utf8")
+        
+        frame_time = datetime.fromtimestamp(event["time"])
+
+        try:
+            event_name = event["variables"]["$event_name"]
+        except:
+            pass
+        
+        event = template_get_event(templates, "traffic", event["variables"])
+        event = frame_time.strftime(event)
+
+        yield bytes(event, "utf8")
