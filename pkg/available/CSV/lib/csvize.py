2603e6e790e88ab5674c239fa2844b62a3161420
Sebastien Tricaud <sebastien.tricaud@devo.com>
2021-11-16 15:49:02 -0800

    Adding CSV package to dump variables in a CSV

---
+++ pkg/available/CSV/lib/csvize.py
@@ -0,0 +1,39 @@
+import sys
+
+from pcraft.LibraryContext import *
+
+class PcraftLogWriter(LibraryContext):
+    def __init__(self):
+        super().__init__()
+        self.is_first = True
+
+    def run(self, event, config, templates):
+        try:
+            fields = event["variables"]["$csv_fields"].split(",")            
+        except:
+            print("Error: CSV plugin requires the $csv_fields variable with coma separated fields for variables we need to extract, such as $csv_fields = \"$foo, $bar\"")
+            sys.exit(1)
+
+        if self.is_first:
+            is_first = False
+            row = ""
+            for f in fields:
+                var = str(f).replace("$", "")
+                var = var.replace("\"", "\\\"")
+                row += var
+                row += ","
+            linerow = row[:len(row)-1] + "\n"
+            yield bytes(str(linerow), "utf8")
+
+        row = ""
+        for f in fields:
+            var = event["variables"][f]
+            var = var.replace("\"", "\\\"")
+            row += var
+            row += ","
+
+        linerow = row[:len(row)-1] + "\n"
+            
+        yield bytes(str(linerow), "utf8")
+        
+        yield None
