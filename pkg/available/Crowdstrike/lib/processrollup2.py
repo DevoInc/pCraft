124638a94b9ce5051984a461dd45e482e3a45c35
Sebastien Tricaud <sebastien.tricaud@devo.com>
2021-12-15 20:19:33 -0800

    Improve automatic variable faking

--- pkg/available/Crowdstrike/lib/processrollup2.py
+++ pkg/available/Crowdstrike/lib/processrollup2.py
@@ -1,3 +1,5 @@
+import random
+
 from datetime import datetime
 
 from pcraft.LibraryContext import *
@@ -10,6 +12,17 @@ class PcraftLogWriter(LibraryContext):
     def run(self, event, config, templates):
         frame_time = datetime.fromtimestamp(event["time"])
 
+        first_pid = random.randint(10000000000000,99000000000000)        
+        event["variables"]["$ParentProcessId"] = str(first_pid)
+        event["variables"]["$SourceProcessId"] = str(first_pid)
+        event["variables"]["$SessionProcessId"] = str(first_pid + random.randint(10, 300))
+        event["variables"]["$ProcessGroupId"] = str(first_pid + random.randint(300, 10000))
+        event["variables"]["$TargetProcessId"] = str(first_pid + random.randint(5000, 30000))
+        
+        event["variables"]["$id"] = self.gen_uuid(event)
+        event["variables"]["$MD5HashData"] = self.gen_md5()
+        event["variables"]["$SHA256HashData"] = self.gen_sha256()
+        
         event = template_get_event(templates, "processrollup2", event["variables"])
         event = frame_time.strftime(event)
         
