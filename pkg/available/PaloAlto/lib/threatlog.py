ff15f0c9993b41ed639c40b62c3e6ed21dbd4334
Sebastien Tricaud <sebastien.tricaud@devo.com>
2021-12-22 09:20:57 -0800

    Can write multiple detections from a single event.

--- pkg/available/PaloAlto/lib/threatlog.py
+++ pkg/available/PaloAlto/lib/threatlog.py
@@ -18,62 +18,44 @@ class PcraftLogWriter(LibraryContext):
         self.seq = random.randint(5000000000000000000,9000000000000000000)
         
     def detect(self, event):
+        variables = {}
+        
         if "$filename" in event["variables"]:
             file_suffix = pathlib.Path(event["variables"]["$filename"]).suffix[1:]
             try:
                 fileinfo = filetype[file_suffix]
-                event["variables"]["$URL/Filename"] = event["variables"]["$filename"]
-                event["variables"]["$Threat/Content Type"] = "wildfire"
-                event["variables"]["$Category"] = fileinfo["category"]
-                event["variables"]["$Threat Id"] = "%s(%s)" % (fileinfo["name"], fileinfo["id"])
-                event["variables"]["$Severity"] = "informational"
-                event["variables"]["$Cloud"] = "wildfire.paloaltonetworks.com"                
-                event["variables"]["$File Type"] = file_suffix
-                event["variables"]["$File Digest"] = str(self.gen_sha256())
-                event["variables"]["$Content Version"] = "WildFire-0"
-                return True
+                variables["$URL/Filename"] = event["variables"]["$filename"]
+                variables["$Threat/Content Type"] = "wildfire"
+                variables["$Category"] = fileinfo["category"]
+                variables["$Threat Id"] = "%s(%s)" % (fileinfo["name"], fileinfo["id"])
+                variables["$Severity"] = "informational"
+                variables["$Cloud"] = "wildfire.paloaltonetworks.com"                
+                variables["$File Type"] = file_suffix
+                variables["$File Digest"] = str(self.gen_sha256())
+                variables["$Content Version"] = "WildFire-0"
+                yield variables
             except:
                 pass
-        
+
+        variables = {}
         if "$uri" in event["variables"]:
-            event["variables"]["$URL/Filename"] = event["variables"]["$uri"]
+            variables["$URL/Filename"] = event["variables"]["$uri"]
             if len(event["variables"]["$uri"]) > 128:
-                event["variables"]["$Threat ID"] = "HTTP GET Requests Long URI Anomaly(30800)"
-                event["variables"]["$Severity"] = "low"
-                event["variables"]["$Category"] = "low-risk"
-                event["variables"]["$Threat/Content Type"] = "vulnerability"
-                event["variables"]["$Threat Category"] = "overflow"                
-                return True
-                
-        return False
-            
-    def run(self, event, config, templates):
-        event_name = "threat"
-        self.seq += 1
+                variables["$Threat ID"] = "HTTP GET Requests Long URI Anomaly(30800)"
+                variables["$Severity"] = "low"
+                variables["$Category"] = "low-risk"
+                variables["$Threat/Content Type"] = "vulnerability"
+                variables["$Threat Category"] = "overflow"                
+                yield variables
 
-        threat_detected = self.detect(event)
-        if not threat_detected:
-            yield None
-            return
-        
-        if self.is_first:
-            self.is_first = False
-            header = template_get_header(templates, event_name)
-            yield bytes(header, "utf8")
-
-        frame_time = datetime.fromtimestamp(event["time"])
-        try:
-            event_name = event["variables"]["$event_name"]
-        except:
-            pass
-
-        event["variables"]["$Threat Category"] = "unknown"                
+    def fill_event(self, event):        
+        event["variables"]["$Threat Category"] = "unknown"
         event["variables"]["$Session ID"] = random.randint(123, 950432)
         event["variables"]["$Type"] = "THREAT"
         event["variables"]["$FUTURE_USE"] = "1"
         event["variables"]["$PCAP_ID"] = "0"
         event["variables"]["$Action"] = "alert"
-        
+                
         packets = event["virtualpackets"]
         first_packet = packets[0]
         last_packet = packets[-1]
@@ -81,14 +63,38 @@ class PcraftLogWriter(LibraryContext):
         bytes_received = last_packet.packet_size
         event["variables"]["$Source Location"] = self.get_country_for_ip(event["variables"]["$Source Address"])
         event["variables"]["$Destination Location"] = self.get_country_for_ip(event["variables"]["$Destination Address"])
-
+        
         event["variables"]["$Source VM UUID"] = self.gen_uuid_fixed(event, first_packet.ip_src)
         event["variables"]["$Destination VM UUID"] = self.gen_uuid_fixed(event, first_packet.ip_dst)
-
+        
         event["variables"]["$Application"] = guess_application(event)
         event["variables"]["$Sequence Number"] = self.seq
+                
+    def run(self, event, config, templates):
+        event_name = "threat"
+
+        self.fill_event(event)
+
+        threats_detected = self.detect(event)
+        for threats in threats_detected:
+            self.seq += 1
+
+            frame_time = datetime.fromtimestamp(event["time"])
+            try:
+                event_name = event["variables"]["$event_name"]
+            except:
+                pass                
+            new_event = event
+            
+            if self.is_first:
+                self.is_first = False
+                header = template_get_header(templates, event_name)
+                yield bytes(header, "utf8")
         
-        logevent = template_get_event(templates, event_name, event["variables"])
-        logevent = frame_time.strftime(logevent)
-        
-        yield bytes(logevent, "utf8")
+            for k,v in threats.items():
+                new_event["variables"][k] = v
+                               
+            logevent = template_get_event(templates, event_name, new_event["variables"])
+            logevent = frame_time.strftime(logevent)
+                
+            yield bytes(logevent, "utf8")
