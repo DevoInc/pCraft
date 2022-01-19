4bd1504ebbc63d21b73cfe37f8b906a3d9f1f74c
Sebastien Tricaud <sebastien.tricaud@devo.com>
2022-01-19 13:50:56 -0800

    Log file generators for each event type

---
+++ pkg/available/Crowdstrike/lib/TokenImpersonated.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+import json
+from datetime import datetime
+
+from pcraft.LibraryContext import *
+from pcraft.TemplateBuilder import template_get_event, template_get_header
+
+class PcraftLogWriter(LibraryContext):
+    def __init__(self):
+        super().__init__()
+
+    def run(self, event, config, templates):
+        frame_time = datetime.fromtimestamp(event["time"])
+
+        event = template_get_event(templates, "TokenImpersonated", event["variables"])
+        event = frame_time.strftime(event)
+        
+        yield bytes(event, "utf8")
