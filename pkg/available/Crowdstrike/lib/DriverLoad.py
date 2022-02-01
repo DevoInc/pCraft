9e07dd62b67adb87f79e7864d99da85b7b680ba3
Sebastien Tricaud <sebastien.tricaud@devo.com>
2022-01-31 22:21:20 -0800

    Adding new events

---
+++ pkg/available/Crowdstrike/lib/DriverLoad.py
@@ -0,0 +1,23 @@
+import random
+
+from datetime import datetime
+
+from pcraft.LibraryContext import *
+from pcraft.TemplateBuilder import template_get_event, template_get_header
+
+from .libcs import *
+
+class PcraftLogWriter(LibraryContext):
+    def __init__(self):
+        super().__init__()
+
+    def run(self, event, config, templates):
+        frame_time = datetime.fromtimestamp(event["time"])
+        
+        event = build_variables(self, event)
+        event = build_pid_variables(self, event)
+        
+        event = template_get_event(templates, "DriverLoad", event["variables"])
+        event = frame_time.strftime(event)
+        
+        yield bytes(event, "utf8")
