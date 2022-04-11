7c7efbe632822939dc63bcd1b95960a615d34d03
Sebastien Tricaud <sebastien.tricaud@devo.com>
2022-04-11 15:56:40 -0700

    Have a random number that then becomes consecutive for RecordNumber

--- pkg/available/WindowsEvents/lib/logwrite.py
+++ pkg/available/WindowsEvents/lib/logwrite.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import json
 from datetime import datetime
+import random
 
 from pcraft.LibraryContext import *
 from pcraft.TemplateBuilder import template_get_event
@@ -8,10 +9,13 @@ from pcraft.TemplateBuilder import template_get_event
 class PcraftLogWriter(LibraryContext):
     def __init__(self):
         super().__init__()
+        self.RecordNumber = random.randint(100000, 110000)
         
     def run(self, event, config, templates):
         frame_time = datetime.fromtimestamp(event["time"])
 
+        event["variables"]["$RecordNumber"] = str(self.RecordNumber)
+        self.RecordNumber += 1
         event = template_get_event(templates, event["variables"]["$event_id"], event["variables"])
         event = frame_time.strftime(event)
 
